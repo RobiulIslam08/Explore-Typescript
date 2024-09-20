4ef628225831c10b568746cbc90d71469d23cda3
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-09-20 14:58:00 +0600

    learnig start typescript

---
+++ index.js
@@ -0,0 +1,4 @@
+var myFuntion = function (age, name) {
+    console.log("my name ".concat(name, " and age ").concat(age));
+};
+myFuntion(20, 'robi');
---
+++ index.ts
@@ -0,0 +1,4 @@
+const myFuntion = (age:number, name:string)=>{
+	console.log(`my name ${name} and age ${age}`)
+}
+myFuntion(20, 'robi')
